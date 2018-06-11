28d031e46ef8cc10dd374c0a3fa75b1d630506c2
jiaoqiqi <1176932165@qq.com>
2018-06-11 15:05:31 +0800

    try mock
    Signed-off-by: jiaoqiqi <1176932165@qq.com>

---
+++ src/services/movieList.js
@@ -0,0 +1,9 @@
+import request from '../utils/request';
+
+export async function queryMovies() {
+  return request(`/movieList`);
+}
+
+// export async function queryMovies(params) {
+//   return request(`/apiv1/problem/list?${stringify(params)}`);
+// }
